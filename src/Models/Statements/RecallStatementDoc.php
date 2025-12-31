f50a72d1c9a8eecb897ba3c146f3ffbd648fbec1
okang <okan.giritli@gmail.com>
2025-12-31 12:43:51 +0200

    chore: Add missing type annotation for `RecallStatementDoc` class

--- src/Models/Statements/RecallStatementDoc.php
+++ src/Models/Statements/RecallStatementDoc.php
@@ -5,6 +5,8 @@ namespace Firebed\AadeMyData\Models\Statements;
 use Firebed\AadeMyData\Models\TypeArray;
 
 /**
+ * @extends TypeArray<RecalledStatementType>
+ *
  * @version 1.0.12
  */
 class RecallStatementDoc extends TypeArray
